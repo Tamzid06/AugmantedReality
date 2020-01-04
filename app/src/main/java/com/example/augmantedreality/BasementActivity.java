077528ad9e59c4787c2c9bf316f4bbc81b8e7214
Tamzid <tamzidhossainxarir@gmail.com>
2020-01-05 02:13:41 +0600

    quiz porjonto

--- app/src/main/java/com/example/augmantedreality/BasementActivity.java
+++ app/src/main/java/com/example/augmantedreality/BasementActivity.java
@@ -4,17 +4,24 @@ import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.augmantedreality.ui.User;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -23,6 +30,8 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class BasementActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
@@ -42,6 +51,7 @@ public class BasementActivity extends AppCompatActivity {
             }
         });
 
+
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -55,6 +65,7 @@ public class BasementActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
     }
 
     @Override
--- app/src/main/java/com/example/augmantedreality/Transition.java
+++ app/src/main/java/com/example/augmantedreality/Transition.java
@@ -1,24 +1,50 @@
 package com.example.augmantedreality;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.augmantedreality.ui.User;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Transition extends AppCompatActivity {
 
+    //FirebaseDatabase.getInstance().getReference().child("Store").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("username").setValue("SIR");
     Button aRButton,showModelButton;
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference myRef;
+    private String userId;
+
+    TextView quiz,ch1,ch2,ch3,ch4,ch5;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transition);
-
-        aRButton = findViewById(R.id.transitionARButton);
-        showModelButton = findViewById(R.id.transitionShowModelButton);
+        load();
+        ch1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Transition.this,Quiz1.class));
+            }
+        });
 
         aRButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,5 +61,15 @@ public class Transition extends AppCompatActivity {
                 //startActivity(new Intent(Transition.this,ShowModelActivity.class));
             }
         });
+    }
+
+    private void load() {
+        aRButton = findViewById(R.id.transitionARButton);
+        showModelButton = findViewById(R.id.transitionShowModelButton);
+        ch1 = findViewById(R.id.chapter1);
+        ch2 = findViewById(R.id.chapter2);
+        ch3 = findViewById(R.id.chapter3);
+        ch4 = findViewById(R.id.chapter4);
+        ch5 = findViewById(R.id.chapter5);
     }
 }
